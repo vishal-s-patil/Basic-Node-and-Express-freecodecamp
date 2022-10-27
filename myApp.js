4dac10359d53630e8f0c8df3968e754bffc6dd83
Vishal Patil <78012271+vishal-s-patil@users.noreply.github.com>
2022-10-27 15:09:19 +0530

    make it better

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
 
-app.use(express.static('public'));
+// app.use(express.static(__dirname + '/public'))
+app.use('/public', express.static(__dirname + '/public'))
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
