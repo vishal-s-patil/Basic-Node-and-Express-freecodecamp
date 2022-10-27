a1fdefc7ea93c7f5d80a79dddb60f97d73dadc68
Vishal Patil <78012271+vishal-s-patil@users.noreply.github.com>
2022-10-27 15:30:52 +0530

    make it better

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 // app.use(express.static(__dirname + '/public'))
 app.use('/public', express.static(__dirname + '/public'))
@@ -11,6 +12,9 @@ app.get('/', (req, res) => {
 
 app.get('/json', (req, res) => {
     let obj = { "message": "Hello json" };
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
+        obj.message = obj.message.toUpperCase();
+    }
     res.json(obj);
 })
 
