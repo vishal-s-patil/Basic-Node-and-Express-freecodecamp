b8a7ae2c78242205a290a46e61e6dcb1ce239300
Vishal Patil <78012271+vishal-s-patil@users.noreply.github.com>
2022-10-27 15:38:39 +0530

    make it better

--- myApp.js
+++ myApp.js
@@ -12,8 +12,11 @@ app.get('/', (req, res) => {
 
 app.get('/json', (req, res) => {
     let obj = { "message": "Hello json" };
-    if (process.env.MESSAGE_STYLE === 'uppercase') {
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
         obj.message = obj.message.toUpperCase();
+    }
+    else {
+        obj.message = 'Hello json';
     }
     res.json(obj);
 })
